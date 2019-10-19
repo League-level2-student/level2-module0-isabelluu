2b0218df5329c67ddcaccf6509b430e848e0d8da
League Student <league@iMac-16.attlocal.net>
2019-10-19 14:25:50 -0700

    Day 2

--- src/arrays/_00_ArrayCheatSheet.java
+++ src/arrays/_00_ArrayCheatSheet.java
@@ -1,27 +1,82 @@
 package arrays;
 
+import java.util.Random;
+
 public class _00_ArrayCheatSheet {
 	public static void main(String[] args) {
 		//1. make an array of 5 Strings
 		
+		String ray[];
+		ray = new String[5];
+		ray[0] = "a";
+		ray[1] = "b";
+		ray[2] = "c";
+		ray[3] = "d";
+		ray[4] = "e";
+		
 		//2. print the third element in the array
-		
+		System.out.println(ray[2]);
 		//3. set the third element to a different value
-		
+		ray[2] = "f";
 		//4. print the third element again
-		
+		//System.out.println(ray[2]);
 		//5. use a for loop to print all the values in the array
 		//   BE SURE TO USE THE ARRAY'S length VARIABLE
-		
+		for(int x = 0; x<5; x++)
+		{
+			System.out.println(ray[x]);
+		}
 		
 		//6. make an array of 50 integers
+		int rays[];
+		rays = new int[50];
 		
 		//7. use a for loop to make every value of the integer array a random number
+		Random num = new Random();
+		for(int i = 0; i<50; i++)
+		{
+			int number = num.nextInt(1000);
+			rays[i] = number;
+		}
 		
-		//8. without printing the entire array, print only the smallest number in the array
+		for(int b = 0; b<50; b++)
+		{
+			System.out.println(rays[b]);
+		}
+		
+		//8. without printing the entire array, print only the smallest number in the array bubble sort
+		for(int c = 0; c < 50; c++)
+		{
+			for(int d =c+1; d<50; d++)
+			{
+				if(rays[c]>rays[d])
+				{
+					int hold = rays[c];
+					rays[c] = rays[d];
+					rays[d] = hold;
+				}
+			}
+		}
+		 
+		System.out.println("/n/n/n");
+		
+		System.out.println(rays[0]);
 		
 		//9 print the entire array to see if step 8 was correct
 		
-		//10. print the largest number in the array.
+		/*for(int b = 0; b<50; b++)
+		{
+			System.out.println(rays[b]);
+		}*/
+		
+		//10. print the largest number in the array. bubble sort
+		
+		System.out.println(rays[49]);
+		
+		
+		
+		
+		
+		
 	}
 }
--- src/arrays/_01_RobotRace.java
+++ src/arrays/_01_RobotRace.java
@@ -1,22 +1,46 @@
 package arrays;
 
+import java.util.Random;
+
+import javax.swing.JOptionPane;
+
+import org.jointheleague.graphical.robot.Robot;
+
 public class _01_RobotRace {
 	//1. make a main method
+public static void main(String[] args) {
 
 	//2. create an array of 5 robots.
-
+	Robot[] rob;
+	rob = new Robot[5];
 	//3. use a for loop to initialize the robots.
+	Robot.setWindowSize(500, 500);
 
+	for(int a = 0; a<5; a++)
+	{
 		//4. make each robot start at the bottom of the screen, side by side, facing up
-
+		rob[a].setY(100);
+		rob[a].setX(400 + 5*a);
+		rob[a].show();
+		
+	}
 	//5. use another for loop to iterate through the array and make each robot move 
 	//   a random amount less than 50.
+	Random num = new Random();
+	for(int x = 0; x<5; x++)
+	{
+		int number = num.nextInt(50);
+		rob[x].setY(number);
+	}
 
 	//6. use a while loop to repeat step 5 until a robot has reached the top of the screen.
-
+		//while(y<)
+	
+	
 	//7. declare that robot the winner and throw it a party!
-
+	JOptionPane.showMessageDialog(null, "You won!");
 	//8. try different races with different amounts of robots.
 
 	//9. make the robots race around a circular track.
+}
 }
